acbbfc7d31e889ccc730b29ed44d3096fb30bea5
Denis Khramov <deniskhramov@gmail.com>
2024-04-15 20:50:20 +0300

    quicksave

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.jpg';
 import './App.css';
+import {Header} from './Header.js';
+import {Nav} from './Nav.js';
+import {Main} from './Main.js';
+import {Footer} from './Footer.js';
 
 function App() {
   return (
     <>
-      <img src={logo}></img>
+      <img src='/logo.jpg' alt="Little Lemon logo"></img>
       <Header/>
       <Nav/>
       <Main/>

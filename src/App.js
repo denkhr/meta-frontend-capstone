52419b2df0a32d895adc037c3308ad62660352b6
Denis Khramov <deniskhramov@gmail.com>
2024-04-28 02:19:53 +0300

    quicksave

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Link } from "react-router-dom";
 import { Main } from './Main.js';
+import lemonImage from './assets/img/lemon-illustation.png';
 
 const Header = () => {
 
@@ -24,8 +25,54 @@ const Header = () => {
 
 const Footer = () => {
   return (
-    <footer>
-      2024 Denis Khramov
+    <footer className="grid-main paddings-section pt-40 pb-40">
+      <div className="h-flex flex-align-start gap-100 col-full">
+
+        <img className="footer_img" src={lemonImage} alt="Little Lemon illustration"></img>
+
+        <div className="v-flex flex-grow-2">
+
+          <div className="h-flex gap-80 flex-align-start">
+
+            <div className="footer_group pb-40">
+              <h2 className="text-h3 color-dark-green pb-8">Doormat Navigation</h2>
+              <ul className="footer_list">
+                <li><a href="#path" className="text-nowrap">Home</a></li>
+                <li><a href="#path" className="text-nowrap">About</a></li>
+                <li><a href="#path" className="text-nowrap">Menu</a></li>
+                <li><a href="#path" className="text-nowrap">Order Online</a></li>
+                <li><a href="#path" className="text-nowrap">Reservations</a></li>
+                <li><a href="#path" className="text-nowrap">Login</a></li>
+              </ul>
+            </div>
+
+            <div className="footer_group pb-40">
+              <h2 className="text-h3 color-dark-green pb-8">Contact</h2>
+              <ul className="footer_list">
+                <li><a href="#path" className="text-nowrap">Adress</a></li>
+                <li><a href="#path" className="text-nowrap">Phone</a></li>
+                <li><a href="#path" className="text-nowrap">Email</a></li>
+              </ul>
+            </div>
+
+            <div className="footer_group pb-40">
+              <h2 className="text-h3 color-dark-green pb-8">Social Media Links</h2>
+              <ul className="footer_list">
+                <li><a href="#path" className="text-nowrap">Facebook</a></li>
+                <li><a href="#path" className="text-nowrap">Instagram</a></li>
+                <li><a href="#path" className="text-nowrap">Twitter</a></li>
+                <li><a href="#path" className="text-nowrap">Youtube</a></li>
+                <li><a href="#path" className="text-nowrap">LinkedIn</a></li>
+              </ul>
+            </div>
+
+          </div>
+
+          <div className="text-legal">2024 © Denis Khramov</div>
+
+        </div>
+
+      </div>
     </footer>
   )
 }
